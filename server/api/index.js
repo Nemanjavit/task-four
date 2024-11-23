590b0132f2541e588280b5bc47fee00e329dfadc
Nemanjavit <vitnemanja3@gmail.com>
2024-11-23 00:03:03 -0600

    change structure

---
+++ server/api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+dotenv.config();
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect();
+
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM users", (err, rows, fields) => {
+    if (err) throw err;
+    console.log(req);
+
+    console.log("The solution is: ", rows);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+connection.end();
