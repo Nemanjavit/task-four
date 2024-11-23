b729617708418a8f779890103b3e7b3287e467d8
Nemanjavit <vitnemanja3@gmail.com>
2024-11-22 23:51:39 -0600

    fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
+dotenv.config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -14,6 +15,7 @@ connection.connect();
 app.get("/users", (req, res) => {
   connection.query("SELECT * FROM users", (err, rows, fields) => {
     if (err) throw err;
+    console.log(req);
 
     console.log("The solution is: ", rows);
   });
